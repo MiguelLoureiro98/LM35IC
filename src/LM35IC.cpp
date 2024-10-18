86629b8192596f146be68fa782311d4323b0bc10
Miguel Loureiro <miguel.santos.loureiro@gmail.com>
2024-10-18 13:15:55 +0100

    Made some changes to the class signature.

--- src/LM35IC.cpp
+++ src/LM35IC.cpp
@@ -3,8 +3,8 @@
 
 namespace LM35{
 
-    LM35IC::LM35IC(uint8_t pin, sensors sensor, double sensitivity, units unit, double Vcc, uint8_t ADC_bits): 
-        _pin(pin), _sensor(sensor), _sensitivity(sensitivity), _unit(unit), _Vcc(Vcc), _adc_res(Vcc / pow(base, ADC_bits)){ 
+    LM35IC::LM35IC(uint8_t pin, sensors sensor, units unit, double Vcc, uint8_t ADC_bits): 
+        _pin(pin), _sensor(sensor), _unit(unit), _adc_res(Vcc / pow(base, ADC_bits)){ 
 
         };     
 
--- src/LM35IC.h
+++ src/LM35IC.h
@@ -6,6 +6,7 @@
 namespace LM35{
 
     constexpr uint8_t base = 2;
+    constexpr double sensitivity = 0.01;
     constexpr uint8_t n_sensors = 5;
     constexpr uint8_t n_details = 8;
 
@@ -17,28 +18,29 @@ namespace LM35{
 
     class LM35IC{
 
+        // Define public methods.
+        public:
+
+            LM35IC(uint8_t pin, sensors sensor=LM35D, units unit=Celcius, double Vcc=5.0, uint8_t ADC_bits=10);
+            double readTemp();
+            double error(double reading, bool worst_case=false);
+            void changeUnits(units unit);
+
         // Define private attributes.
         private:
 
             uint8_t _pin;
             units _unit;
             sensors _sensor;
-            double _sensitivity;
+            uint8_t _adc_res;
+            double _gain;
+            double _offset;
             double _sensors[n_sensors][n_details] = {{0.0, 100.0, 0.012, 0.6, 0.004, 0.02, 1.5, 0.0067},
                                                     {-55.0, 150.0, 0.005, 0.4, 0.0032, 0.00625, 1.0, 0.004},
                                                     {-55.0, 150.0, 0.0025, 0.2, 0.0016, 0.00625, 0.5, 0.004},
                                                     {-40.0, 110.0, 0.003077, 0.2, 0.002353, 0.0154, 0.5, 0.005882},
                                                     {-40.0, 110.0, 0.006154, 0.4, 0.004706, 0.0154, 1.0, 0.005882}};
-            double _Vcc;
-            uint8_t _adc_res;
-
-        public:
-
-            LM35IC(uint8_t pin, sensors sensor=LM35D, double sensitivity=0.01, units unit=Celcius, double Vcc=5.0, uint8_t ADC_bits=10);
-            double readTemp();
-            void changeUnits(units unit);
-            double accuracy(double reading);
-            double resolution();
+            double _res = 0.0;
 
     };
 };
