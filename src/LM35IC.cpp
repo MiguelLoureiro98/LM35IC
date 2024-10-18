576a206812c5038441cc31a5a819bee66684aa5f
Miguel Loureiro <miguel.santos.loureiro@gmail.com>
2024-10-18 17:59:59 +0100

    First version of the class' API.

--- src/LM35IC.cpp
+++ src/LM35IC.cpp
@@ -4,8 +4,67 @@
 namespace LM35{
 
     LM35IC::LM35IC(uint8_t pin, sensors sensor, units unit, double Vcc, uint8_t ADC_bits): 
-        _pin(pin), _sensor(sensor), _unit(unit), _adc_res(Vcc / pow(base, ADC_bits)){ 
+        _pin(pin), _sensor(sensor), _unit(unit), _adc_res(Vcc / pow(2.0, ADC_bits)){ 
 
         };     
 
-}
+    double LM35IC::readTemp(){
+
+        double adc_reading = 0.0;
+
+        return 1.0 / _sensitivity * (1.0 / _gain * adc_reading * _adc_res + _offset);
+
+    };
+
+    double LM35IC::error(double temperature, bool worst_case){
+
+        return _computeAccuracy(temperature, worst_case) + _adc_accuracy * _resolution;
+
+    };
+
+    void LM35IC::changeUnits(units unit){
+
+        _unit = unit;
+
+    };
+
+    double LM35IC::_computeAccuracy(double temperature, bool worst_case){
+
+        const bool above_reference_temperature = temperature < _reference_temperature ? false : true;
+        const int intercept_index = _intercept_indices[worst_case];
+        const int slope_index = _slope_indices[2 * worst_case + above_reference_temperature];
+
+        return _sensors[_sensor][slope_index] * temperature + _sensors[_sensor][intercept_index]; 
+
+    };
+
+    double LM35IC::_computeResolution(){
+
+        double sensor_range = _sensors[_sensor][1] - _sensors[_sensor][0];
+
+        return (sensor_range) / (1.0 / _adc_res * _gain * _sensitivity * (sensor_range));
+
+    };
+
+    double LM35IC::_Celsius2Kelvin(double temperature_Celsius){
+
+        return temperature_Celsius + 273.15;
+
+    };
+
+    double LM35IC::_Celsius2Fahrenheit(double temperature_Celsius){
+
+        return 9.0 / 5.0 * temperature_Celsius + 32.0;
+
+    };
+
+    constexpr double LM35IC::_sensitivity;
+    constexpr double LM35IC::_reference_temperature;
+    constexpr double LM35IC::_adc_accuracy;
+    constexpr uint8_t LM35IC::_n_sensors;
+    constexpr uint8_t LM35IC::_n_details;
+    constexpr double LM35IC::_sensors[_n_sensors][_n_details];
+    constexpr uint8_t LM35IC::_slope_indices[4];
+    constexpr uint8_t LM35IC::_intercept_indices[2];
+
+};
--- src/LM35IC.h
+++ src/LM35IC.h
@@ -5,13 +5,8 @@
 
 namespace LM35{
 
-    constexpr uint8_t base = 2;
-    constexpr double sensitivity = 0.01;
-    constexpr uint8_t n_sensors = 5;
-    constexpr uint8_t n_details = 8;
-
     // Define units type.
-    enum units{Celcius=0, Kelvin, Fahrenheit};
+    enum units{Celsius=0, Kelvin, Fahrenheit};
 
     // Define sensors type.
     enum sensors{LM35, LM35A, LM35CA, LM35C, LM35D=0};
@@ -21,10 +16,18 @@ namespace LM35{
         // Define public methods.
         public:
 
-            LM35IC(uint8_t pin, sensors sensor=LM35D, units unit=Celcius, double Vcc=5.0, uint8_t ADC_bits=10);
+            LM35IC(uint8_t pin, sensors sensor=LM35D, units unit=Celsius, double Vcc=5.0, uint8_t ADC_bits=10);
             double readTemp();
-            double error(double reading, bool worst_case=false);
+            double error(double temperature, bool worst_case=false);
             void changeUnits(units unit);
+
+        // Define private methods.
+        private:
+
+            double _computeResolution();
+            double _computeAccuracy(double temperature, bool worst_case);
+            double _Celsius2Kelvin(double temperature_Celsius);
+            double _Celsius2Fahrenheit(double temperature_Celsius);
 
         // Define private attributes.
         private:
@@ -35,12 +38,19 @@ namespace LM35{
             uint8_t _adc_res;
             double _gain;
             double _offset;
-            double _sensors[n_sensors][n_details] = {{0.0, 100.0, 0.012, 0.6, 0.004, 0.02, 1.5, 0.0067},
-                                                    {-55.0, 150.0, 0.005, 0.4, 0.0032, 0.00625, 1.0, 0.004},
-                                                    {-55.0, 150.0, 0.0025, 0.2, 0.0016, 0.00625, 0.5, 0.004},
-                                                    {-40.0, 110.0, 0.003077, 0.2, 0.002353, 0.0154, 0.5, 0.005882},
-                                                    {-40.0, 110.0, 0.006154, 0.4, 0.004706, 0.0154, 1.0, 0.005882}};
-            double _res = 0.0;
+            static constexpr double _sensitivity = 0.01;
+            static constexpr double _reference_temperature = 25.0;
+            static constexpr double _adc_accuracy = 2.0;
+            static constexpr uint8_t _n_sensors = 5;
+            static constexpr uint8_t _n_details = 8;
+            static constexpr double _sensors[_n_sensors][_n_details] = {{0.0, 100.0, 0.012, 0.6, 0.004, 0.02, 1.5, 0.0067},
+                                                                        {-55.0, 150.0, 0.005, 0.4, 0.0032, 0.00625, 1.0, 0.004},
+                                                                        {-55.0, 150.0, 0.0025, 0.2, 0.0016, 0.00625, 0.5, 0.004},
+                                                                        {-40.0, 110.0, 0.003077, 0.2, 0.002353, 0.0154, 0.5, 0.005882},
+                                                                        {-40.0, 110.0, 0.006154, 0.4, 0.004706, 0.0154, 1.0, 0.005882}};
+            static constexpr uint8_t _slope_indices[4] = {2, 4, 5, 7};
+            static constexpr uint8_t _intercept_indices[2] = {3, 6};
+            double _resolution = _computeResolution();
 
     };
 };
