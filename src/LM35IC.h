3151776e070e714afe382a65099cc81047b4a2aa
Miguel Loureiro <miguel.santos.loureiro@gmail.com>
2024-10-28 12:42:09 +0000

    Finished documenting the code using Doxygen comments.

--- src/LM35IC.h
+++ src/LM35IC.h
@@ -22,27 +22,140 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.   
 ******************************************************************************/
 
+/*****************************************************************************
+*
+* @file This file contains the declarations of every class and type belonging 
+*  the LM35 namespace.
+*
+******************************************************************************/
+
 #ifndef _LM35IC_H_
 #define _LM35IC_H_
 
 #include <stdint.h>
 
+/******************************************************************************
+* @namespace LM35
+* 
+* @brief The library's namespace.
+*
+* @details A namespace containing the sensors type and the LM35IC class, both 
+* of which are defined by the library.
+*******************************************************************************/
 namespace LM35{
 
-    // Define sensors type.
+    /*******************************************************************************
+    * @enum sensors
+    * 
+    * @brief Sensors supported by this library.
+    * 
+    * @details This type defines every sensor supported by this library: LM35_normal, 
+    * LM35A, LM35CA, LM35C, and LM35D. Read the sensors' data sheet for more details.
+    * 
+    ********************************************************************************/
     enum sensors{LM35_normal, LM35A, LM35CA, LM35C, LM35D};
 
+    /*******************************************************************************
+    *
+    * @class LM35IC
+    * 
+    * @brief LM35 sensor class.
+    * 
+    * @details This class implements the operations associated with the sensor. 
+    * These include collecting a sample, unit conversion, and computing measurement 
+    * error, sensor accuracy and measurement chain resolution.
+    * 
+    ********************************************************************************/
     class LM35IC{
 
         // Define public methods.
         public:
 
+            /*******************************************************************************
+            * 
+            * @brief Class constructor.
+            * 
+            * @param[in] pin Analog input pin connected to the sensor's output.
+            * @param[in] sensor Sensor type.
+            * @param[in] gain Signal conditioning circuit gain.
+            * @param[in] offset Signal conditioning circuit offset.
+            * @param[in] Vcc Arduino board operating voltage.
+            * @param[in] ADC_bits Number of bits of the Analog-to-Digital Converter.
+            * 
+            ********************************************************************************/
             LM35IC(uint8_t pin, sensors sensor=LM35D, double gain=1.0, double offset=0.0, double Vcc=5.0, uint8_t ADC_bits=10);
+
+            /*******************************************************************************
+            *
+            * @brief Collect a sensor reading and convert it to a temperature value.
+            * 
+            * @return Temperature value in Celsius.
+            * 
+            ********************************************************************************/
             double readTemp() const;
+
+            /*******************************************************************************
+            *
+            * @brief Compute an error estimate for a given measurement.
+            * 
+            * @details This method yields an estimate of the total measurement error. It takes 
+            * into account the sensor's accuracy, the measurement's resolution, and the ADC 
+            * noise. The method uses the LM35IC::computeAccuracy and LM35IC::getResolution to 
+            * compute the sensor's accuracy and the measurement's resolution, respectively.
+            * 
+            * @param[in] temperature Temperature measurement in Celsius.
+            * @param[in] worst_case Accuracy curve that should be used. 'true' for worst-case
+            *   curve, 'false' for typical curve.
+            * 
+            * @return Error estimate in Celsius.
+            * 
+            ********************************************************************************/
             double computeError(double temperature, bool worst_case=false) const;
+
+            /*******************************************************************************
+            *
+            * @brief Get the measurements' resolution.
+            * 
+            * @return Measurement resolution.
+            * 
+            * @see LM35IC::computeError
+            ********************************************************************************/
             double getResolution() const;
+
+            /*******************************************************************************
+            *
+            * @brief Compute an estimate of the sensor's accuracy for a given estimate.
+            * 
+            * @param[in] temperature Temperature measurement in Celsius.
+            * @param[in] worst_case Accuracy curve that should be used. 'true' for worst-case
+            *   curve, 'false' for typical curve.
+            * 
+            * @return Sensor accuracy for the given measurement.
+            * 
+            * @see LM35IC::computeError
+            ********************************************************************************/
             double computeAccuracy(double temperature, bool worst_case=false) const;
+
+            /*******************************************************************************
+            *
+            * @brief Convert a temperature value from Celsius to Kelvin.
+            * 
+            * @param[in] temperature_Celsius Temperature in Celsius.
+            * 
+            * @return Temperature in Kelvin.
+            * 
+            ********************************************************************************/
             static double Celsius2Kelvin(double temperature_Celsius);
+
+            /*******************************************************************************
+            *
+            * @brief Convert a temperature value from Celsius to Fahrenheit.
+            * 
+            * @param[in] temperature_Celsius Temperature in Celsius.
+            * 
+            * @return Temperature in Fahrenheit.
+            * 
+            ********************************************************************************/
             static double Celsius2Fahrenheit(double temperature_Celsius);
 
         // Define private methods.
